bc87e2fb54bc95c18d06207b352af181c99cc6ca
rogeralvarez3 <rogeralvarez3@gmail.com>
2022-08-11 17:54:31 +0200

    empleados terminado

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,37 @@
 <template>
-  <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav>
-    <router-view />
-  </div>
+  <v-app>
+    <v-navigation-drawer app clipped dense v-model="drawer">
+      <v-list>
+        <v-list-item v-for="mn in menu" :key="mn.id">
+          <v-list-item-icon>
+            <v-icon v-text="mn.icon"></v-icon>
+          </v-list-item-icon>
+          <v-list-item-title v-text="mn.title"></v-list-item-title>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+    <v-app-bar app color="primary" dark dense clipped-left>
+      <v-app-bar-nav-icon v-if="!drawer"></v-app-bar-nav-icon>
+      <v-app-bar-title>WEBVentas</v-app-bar-title>
+      <v-spacer></v-spacer>
+    </v-app-bar>
+    <v-main>
+      <router-view />
+    </v-main>
+  </v-app>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+<script>
+export default {
+  name: "App",
+  data: () => ({
+    //
+    drawer: null,
+    menu: [
+      { id: 1, title: "Empleados", icon: "mdi-account" },
+      { id: 1, title: "Ventas", icon: "mdi-cart" },
+      { id: 1, title: "Opciones", icon: "mdi-cog" },
+    ],
+  }),
+};
+</script>
